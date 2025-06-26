55a0f486111c9731b33beba3299124d5b49f18b3
Eric Zhang <124303097+CheckAndMate00@users.noreply.github.com>
2025-06-25 23:38:28 -0400

    Update lang.py

--- sap-copilot/backend/lang.py
+++ sap-copilot/backend/lang.py
@@ -1,7 +1,6 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Directly set the key (only in dev environments!)
 os.environ["GOOGLE_API_KEY"] = "AIzaSyAp0Xyg4phu2ueldXMfw7U3UxhgOHriIyc"
 
 chat_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
